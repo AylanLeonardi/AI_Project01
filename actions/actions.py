beefbc7fb2fc6b746940195e5c3e7e712a1bf2da
Aylan Leonardi <leonardi.aylan@gmail.com>
2025-05-27 17:41:45 -0300

    actions.py

--- actions/actions.py
+++ actions/actions.py
@@ -38,22 +38,41 @@ class Action_Descricao_Entrega(Action):
             dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]):
-        texto = "Pedido recebido: Você deseja receber:"    
+        
+        texto = "Pedido recebido: Você deseja receber:"
+        
         jogo_nomes = tracker.get_slot('nome_jogo_slot')
         num = tracker.get_slot('qtd_unidades_slot')
         endereco = tracker.get_slot('endereco_slot')
+
+        # Dicionário para converter números escritos em números inteiros em string
+        number_map = {
+            "um": "1", "uma": "1", "dois": "2", "duas": "2", "três": "3",
+            "quatro": "4", "cinco": "5", "seis": "6", "sete": "7", "oito": "8",
+            "nove": "9", "dez": "10", "onze": "11", "doze": "12", "treze": "13",
+            "quatorze": "14", "quinze": "15"
+        }
+
+        # Converter números escritos para dígitos
+        if num is not None:
+            num = [number_map.get(n.lower(), n) for n in num]
+
         # Caso sejam detectados mais sabores do que qtdes:
-        while(len(jogo_nomes)>len(num)):
-            num.append('uma')
+        while len(jogo_nomes) > len(num):
+            num.append('1')  # adicionar '1' para quantidade padrão
+
         # Caso sejam detectados mais qtdes do que sabores:
-        while(len(jogo_nomes)<len(num)):
-            qtdes.pop(0)
-        for n, nome in zip(num,jogo_nomes):
-            if(n != "uma" and n != "1"):
-                texto = texto + "\n - "+ str(n) + " unidade(s) do " + str(nome)
+        while len(jogo_nomes) < len(num):
+            num.pop(0)
+
+        for n, nome in zip(num, jogo_nomes):
+            if n != "1":
+                texto += f"\n - {n} unidade(s) do {nome}"
             else:
-                texto = texto + "\n - "+ str(n) + " unidade do " + str(nome)
-        texto = texto + '\nO endereço fornecido foi: ' + str(endereco) + '. Favor confirmar com sim ou não.'
+                texto += f"\n - {n} unidade do {nome}"
+
+        texto += f'\nO endereço fornecido foi: {endereco}. Favor confirmar com sim ou não.'
+
         dispatcher.utter_message(text=texto)
         return []
 
